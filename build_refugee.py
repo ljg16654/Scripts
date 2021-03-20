1cc77c3daf160aee1aeca10118f2524755d2906b
Jigang Li <ljg16654@sjtu.edu.cn>
2021-03-20 23:50:21 +0800

    build_refugee: dump windows in one monitor to another

---
+++ build_refugee.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import sys
+import subprocess
+
+if len(sys.argv) != 3:
+    print("usage: ", str(sys.argv[0]), "monitor_to_remove monitor_safe")
+    raise RuntimeError("Wrong number of arguments")
+
+# number of existing refugee camps
+desktop_names=subprocess.run(["bspc", "query", "--names", "-D"],
+                             stdout=subprocess.PIPE).stdout.decode()
+
+if "refugee_camp" in desktop_names:
+    raise RuntimeError("refugee_camps already exists!")
+
+ids_stdout= subprocess.run(["bspc", "query", "-N", "-m", str(sys.argv[1])],
+                           stdout=subprocess.PIPE).stdout.decode().split("\n")[:-1]
+
+subprocess.run(["bspc", "monitor", str(sys.argv[2]), "-a", "refugee_camp"])
+
+for id in ids_stdout:
+    subprocess.run(["bspc", "node", id, "-d", "refugee_camp"])
