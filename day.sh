e288a772467ca8df09f89e805a3f8e5c27631684
Jigang Li <ljg16654@sjtu.edu.cn>
2021-06-05 13:24:18 +0800

    update readme

--- day.sh
+++ day.sh
@@ -3,4 +3,5 @@
 hsetroot -solid "#c5e1a5"
 operandi='(load-theme `modus-operandi t)'
 emacsclient -e $operandi
-
+bspc config normal_border_color "#ffffff"
+bspc config focused_border_color "#000000"
--- night.sh
+++ night.sh
@@ -3,3 +3,5 @@
 hsetroot -solid "#001004"
 vivendi='(load-theme `modus-vivendi t)'
 emacsclient -e $vivendi
+bspc config normal_border_color "#000000"
+bspc config focused_border_color "#ffffff"
